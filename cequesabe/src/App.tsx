6596a679fa66fdb7c717378c8453706063d118b0
oDaviML <85958184+oDaviML@users.noreply.github.com>
2023-07-06 11:15:01 -0300

    .

---
+++ cequesabe/src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Nossos Albuns Favoritos</h1>
+      <div id='albuns'>
+        <div className='container'>
+          <h3>Revelação</h3>
+          <img src="https://upload.wikimedia.org/wikipedia/pt/3/30/Ao_Vivo_no_Morro.jpg" alt="" />
+          <h2>Ao vivo no morro</h2>
+          <p><strong>Ano lançamento:</strong> 2009</p>
+          <p><strong>Gravadora:</strong> Deckdisc</p>
+          <p><strong>Duração</strong> 1h20</p>
+          <a href="https://www.youtube.com/watch?v=Iy1dXAcuK7o" target='_blank'><img id='play' src="https://cdn-icons-png.flaticon.com/512/0/375.png" alt="" /></a>
+        </div>
+
+        <div className='container'>
+          <h3>Marília Mendonça</h3>
+          <img src="https://upload.wikimedia.org/wikipedia/pt/0/0a/Mar%C3%ADlia_Mendon%C3%A7a_-_2019_-_Todos_os_Cantos%2C_Vol._1.jpg" alt="" />
+          <h2>Todos os Cantos</h2>
+          <p><strong>Ano lançamento:</strong> 2019</p>
+          <p><strong>Gravadora:</strong> Som Livre</p>
+          <p><strong>Duração</strong> 33min</p>
+          <a href="https://www.youtube.com/playlist?list=PLogA95SVK3hPZ8Hb0I_hRnx50_MRkcE0M" target='_blank'><img id='play' src="https://cdn-icons-png.flaticon.com/512/0/375.png" alt="" /></a>
+        </div>
+
+        <div className='container'>
+          <h3>Joji</h3>
+          <img src="https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/31/89/98/318998f5-81e2-b6d9-2808-9de4582b6e7a/093624864387.jpg/1200x1200bb.jpg" alt="" />
+          <h2>Smithereens</h2>
+          <p><strong>Ano lançamento:</strong> 2022</p>
+          <p><strong>Gravadora:</strong> Warner Records</p>
+          <p><strong>Duração</strong> 25min</p>
+          <a href="https://www.youtube.com/watch?v=elmMB3N7D74" target='_blank'><img id='play' src="https://cdn-icons-png.flaticon.com/512/0/375.png" alt="" /></a>
+        </div>
+      </div>
+      
+      <div id='albuns'>
+        <div className='container'>
+          <h3>Taylor Swift</h3>
+          <img src="https://akamai.sscdn.co/uploadfile/letras/albuns/a/5/c/f/1398201666349375.jpg " alt="" />
+          <h2>Midnights</h2>
+          <p><strong>Ano lançamento:</strong> 2022</p>
+          <p><strong>Gravadora:</strong> Universal Republic Records</p>
+          <p><strong>Duração</strong> 44min</p>
+          <a href="https://www.youtube.com/playlist?list=PLmU8B4gZ41ieaeZUuOH2iUj0Xaw5ELnGH" target='_blank'><img id='play' src="https://cdn-icons-png.flaticon.com/512/0/375.png" alt="" /></a>
+        </div>
+
+        <div className='container'>
+          <h3>Ed Sheeran</h3>
+          <img src="https://m.media-amazon.com/images/I/81oMqLRXxML._UF1000,1000_QL80_.jpg" alt="" />
+          <h2>Divide</h2>
+          <p><strong>Ano lançamento:</strong> 2017</p>
+          <p><strong>Gravadora:</strong>Asylum</p>
+          <p><strong>Duração</strong> 1hr</p>
+          <a href="https://www.youtube.com/watch?v=OEFWiqiHxcI" target='_blank'><img id='play' src="https://cdn-icons-png.flaticon.com/512/0/375.png" alt="" /></a>
+        </div>
+
+        <div className='container'>
+          <h3>Chase Atlantic</h3>
+          <img src="https://i.scdn.co/image/ab67616d0000b2735a0c2870f4f309e382d1fad6" alt="" />
+          <h2>Chase Atlantic</h2>
+          <p><strong>Ano lançamento:</strong> 2017</p>
+          <p><strong>Gravadora:</strong> Warner Records</p>
+          <p><strong>Duração</strong> 25min</p>
+          <a href="https://www.youtube.com/playlist?list=PLU0zsCmSUekaPgHz6ZWeSpewQ6n67aTDM" target='_blank'><img id='play' src="https://cdn-icons-png.flaticon.com/512/0/375.png" alt="" /></a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
